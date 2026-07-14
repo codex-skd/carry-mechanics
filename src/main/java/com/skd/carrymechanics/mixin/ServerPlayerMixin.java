487f94580fba3f2bfe030b58e03773a415aeb933
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-14 18:35:35 +0200

    fix: prevent jump via ServerPlayer.jumpFromGround mixin instead of LivingEntity
    v0.0.0-beta.10

--- src/main/java/com/skd/carrymechanics/mixin/ServerPlayerMixin.java
+++ src/main/java/com/skd/carrymechanics/mixin/ServerPlayerMixin.java
@@ -1,22 +1,20 @@
 package com.skd.carrymechanics.mixin;
 
 import com.skd.carrymechanics.carry.CarryDataManager;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+@Mixin(ServerPlayer.class)
+public class ServerPlayerMixin {
 
     @Inject(method = "jumpFromGround", at = @At("HEAD"), cancellable = true)
     private void onJumpFromGround(CallbackInfo ci) {
-        if ((Object) this instanceof Player player) {
-            if (CarryDataManager.getCarryData(player).isCarrying()) {
-                ci.cancel();
-            }
+        ServerPlayer self = (ServerPlayer) (Object) this;
+        if (CarryDataManager.getCarryData(self).isCarrying()) {
+            ci.cancel();
         }
     }
 }

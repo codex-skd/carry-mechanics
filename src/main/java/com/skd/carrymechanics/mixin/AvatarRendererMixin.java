e7f2c1947235fa2c57be5383d19e9022d33ecadc
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-14 18:44:54 +0200

    feat: implement block rendering in first-person hand via BlockModelRenderState
    v0.0.0-beta.11

--- src/main/java/com/skd/carrymechanics/mixin/AvatarRendererMixin.java
+++ src/main/java/com/skd/carrymechanics/mixin/AvatarRendererMixin.java
@@ -5,9 +5,14 @@ import com.skd.carrymechanics.carry.CarryData;
 import com.skd.carrymechanics.carry.CarryDataManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.BlockModelRenderState;
+import net.minecraft.client.renderer.block.model.BlockDisplayContext;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,8 +31,21 @@ public class AvatarRendererMixin {
         CarryData data = CarryDataManager.getCarryData(player);
         if (!data.isCarrying()) return;
 
-        // TODO: Render carried object using SubmitNodeCollector API
-        // This Minecraft version uses a completely new render system (SubmitNodeCollector)
-        // Actual rendering will be implemented in a future update
+        if (data.isCarrying(CarryData.CarryType.BLOCK)) {
+            var state = data.getBlock();
+            if (state.isAir()) return;
+
+            var resolver = Minecraft.getInstance().getBlockModelResolver();
+            var renderState = new BlockModelRenderState();
+            resolver.update(renderState, state, BlockDisplayContext.create());
+
+            if (!renderState.isEmpty()) {
+                poseStack.pushPose();
+                poseStack.translate(0.1, -0.3, -0.5);
+                poseStack.scale(0.5f, 0.5f, 0.5f);
+                renderState.submit(poseStack, collector, lightCoords, OverlayTexture.NO_OVERLAY, 0);
+                poseStack.popPose();
+            }
+        }
     }
 }

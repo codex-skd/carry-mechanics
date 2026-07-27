46a4468c7ff13188151b9ea5881c2dfd8daccccd
Mods Minecraft CI <ci@mods-minecraft.dev>
2026-07-27 15:07:59 +0000

    chore: sync public code from 2b7992d6

--- src/main/java/com/skd/carrymechanics/client/render/CarryingItemRenderLayer.java
+++ src/main/java/com/skd/carrymechanics/client/render/CarryingItemRenderLayer.java
@@ -1,6 +1,7 @@
 package com.skd.carrymechanics.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import com.skd.carrymechanics.carry.CarryData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.player.PlayerModel;
@@ -30,8 +31,9 @@ public class CarryingItemRenderLayer extends RenderLayer<AvatarRenderState, Play
             if (blockState.isAir()) return;
 
             poseStack.pushPose();
-            poseStack.translate(-0.3, -(renderState.boundingBoxHeight + 0.2), -0.3);
-            poseStack.scale(0.6f, 0.6f, 0.6f);
+            poseStack.translate(0.0, -(renderState.boundingBoxHeight - 0.1), 0.0);
+            poseStack.mulPose(Axis.YP.rotationDegrees(180));
+            poseStack.scale(0.5f, 0.5f, 0.5f);
 
             var resolver = Minecraft.getInstance().getBlockModelResolver();
             var blockRenderState = new BlockModelRenderState();
